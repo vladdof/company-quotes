14b81c58c8630a13daf806ef0ffabb25a5048ce6
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-05-27 16:57:53 +0200

    refactor: change routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import { APP_ROUTES } from '@/constants';
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
-]
+    path: APP_ROUTES.ROOT.PATH,
+    name: APP_ROUTES.ROOT.NAME,
+    component: () => import(
+      /* webpackChunkName: "page-stocks" */
+      '@/views/page-stocks.vue'
+    ),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
