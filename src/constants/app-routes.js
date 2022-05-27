8f0aae56cb649d73511a3d08e3a201b3d858ee7d
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-05-27 16:54:57 +0200

    feat: add app-router constant

---
+++ src/constants/app-routes.js
@@ -0,0 +1,13 @@
+/**
+ * All routes path,name available in application
+ */
+export default {
+    ROOT: {
+        PATH: '/',
+        NAME: 'page-stocks',
+    },
+    STOCK: {
+        PATH: '/stock',
+        NAME: 'page-stock',
+    },
+};
