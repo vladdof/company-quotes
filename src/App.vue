ccd4add15543697b49816a0d973f483e1c1a6199
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-05-27 16:53:04 +0200

    feat: change layout app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Stocks</router-link>
-  </div>
   <router-view/>
 </template>
 
-<style lang="scss">
-
-</style>
-
 <script>
-  // import Stocks from '@/views/Stocks'
-
 export default {
   name: 'App',
-  components: {
-    // Stocks
-  }
-}
+};
 </script>
