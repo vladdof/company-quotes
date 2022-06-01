dd7664ae4f00087ee5818a8f30cd96626d455149
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-06-01 17:00:05 +0200

    feat: add config file

---
+++ src/config.js
@@ -0,0 +1,18 @@
+export default {
+  apiUrl: 'https://financialmodelingprep.com/api',
+  apiKey: 'd8ed336ddcddad19f8ed0ab6333e6193',
+  companies: [
+    'BABA',
+    'AAPL',
+    'FB',
+    'GOOG',
+    'ADBE',
+    'AMZN',
+    'HOG',
+    'CSCO',
+    'DIS',
+    'KO',
+    'INTC',
+    'MA',
+  ]
+};
