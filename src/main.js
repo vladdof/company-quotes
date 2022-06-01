f8cac6ea634ff40b5592a14e8d6ece8a68690234
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-06-01 16:01:53 +0200

    feat: add store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import './assets/main.scss'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from '@/router';
+import store from './store';
+import './assets/main.scss';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(router).use(store).mount('#app');
