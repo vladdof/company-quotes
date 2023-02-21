3bdd45a86f37638f570097c9a45f584c61963627
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-02-21 21:09:23 +0100

    feat: add lang=ts attr for vue files

--- src/components/list-item/list-item.vue
+++ src/components/list-item/list-item.vue
@@ -1,62 +1,64 @@
 <template>
-    <ul class="list-item">
-      <li
-        v-for="(value, key) in item"
-        :key="key"
-        class="list-item__content"
+  <ul class="list-item">
+    <li
+      v-for="(value, key) in item"
+      :key="key"
+      class="list-item__content"
+    >
+      <div
+        v-if="key !== 'image'"
+        class="list-item__label"
       >
-        <div
-            v-if="key !== 'image'"
-            class="list-item__label"
-        >
-            {{ key }}
-        </div>
+        {{ key }}
+      </div>
 
-        <div
-            v-if="key === 'price'"
-            class="list-item__description"
-        >
-            ${{ value }}
-        </div>
+      <div
+        v-if="key === 'price'"
+        class="list-item__description"
+      >
+        ${{ value }}
+      </div>
 
-        <a
-            v-else-if="key === 'website'"
-            class="list-item__link"
-            :href="value"
-            target="_blank"
-        >
-            {{ value }}
-        </a>
+      <a
+        v-else-if="key === 'website'"
+        class="list-item__link"
+        :href="value"
+        target="_blank"
+      >
+        {{ value }}
+      </a>
 
-        <img
-            v-else-if="key === 'image'"
-            class="list-item__img"
-            :src="value"
-            :alt="item.symbol"
-        />
+      <img
+        v-else-if="key === 'image'"
+        class="list-item__img"
+        :src="value"
+        :alt="item.symbol"
+      />
 
-        <div
-            v-else
-            class="list-item__description"
-        >
-            {{ value }}
-        </div>
-      </li>
-    </ul>
+      <div
+        v-else
+        class="list-item__description"
+      >
+        {{ value }}
+      </div>
+    </li>
+  </ul>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
   name: 'list-item',
   props: {
     item: {
-        type: Object,
-        default: () => {},
+      type: Object,
+      default: ({}),
     },
   },
-};
+});
 </script>
 
 <style lang="scss">
-    @import 'list-item';
+  @import 'list-item';
 </style>
--- src/components/stocks/stocks-item.vue
+++ src/components/stocks/stocks-item.vue
@@ -1,34 +1,32 @@
 <template>
-    <div class="stock-item">
-        <div class="stock-item__info">
-            <div class="stock-item__cover">
-                <img
-                    :src="item.image"
-                    :alt="item.companyName"
-                >
-                <div class="stock-item__price">
-                    ${{ item.price }}
-                </div>
-            </div>
-            <div>
-                <h4 class="stock-item__title">
-                    {{ item.companyName }}
-                </h4>
-                <span class="stock-item__symbol">
-                    {{ item.symbol }}
-                </span>
-            </div>
-        </div>
+  <div class="stock-item">
+    <div class="stock-item__info">
+      <div class="stock-item__cover">
+        <img :src="item.image" :alt="item.companyName" />
+        <div class="stock-item__price">${{ item.price }}</div>
+      </div>
+      <div>
+        <h4 class="stock-item__title">
+          {{ item.companyName }}
+        </h4>
+        <span class="stock-item__symbol">
+          {{ item.symbol }}
+        </span>
+      </div>
     </div>
+  </div>
 </template>
 
-<script>
-export default {
-    name: 'stock-item',
-    props: {
-        item: {
-            type: Object,
-        },
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'stock-item',
+  props: {
+    item: {
+      type: Object,
+      default: ({}),
     },
-};
+  },
+});
 </script>
