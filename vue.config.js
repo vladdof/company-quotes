53b5c23e341beb88720ca41d850b7390b15c05bd
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-03-01 20:43:04 +0100

    chore: chnage vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/company-quotes/'
-    : '/'
+  publicPath:
+    process.env.NODE_ENV === 'render'
+    ? '/' : (
+      process.env.NODE_ENV === 'production'
+        ? '/company-quotes/'
+        : '/'
+    )
 }
